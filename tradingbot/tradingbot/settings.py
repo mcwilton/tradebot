3583744fd3714ab846884716d64fd5f1dbe90d27
McWilton <mcwilton85@gmail.com>
2022-04-08 11:08:08 +0400

    Update settings.py

--- tradingbot/tradingbot/settings.py
+++ tradingbot/tradingbot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jtlqh_m6sryloq=p))@h#umx!0w&8ldz1qark2&^$x@72bok8p'
+# SECRET_KEY = 'django-insecure-jtlqh_m6sryloq=p))@h#umx!0w&8ldz1qark2&^$x@72bok8p'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
